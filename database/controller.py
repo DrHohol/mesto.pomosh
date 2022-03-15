e7e9508bf198bee1928c4e9caa6e23c96a6255d9
Sautin Dmytro <dimonsova1488@gmail.com>
2022-03-15 12:45:26 +0200

    add and edit users, add and edit drives

--- database/controller.py
+++ database/controller.py
@@ -1,34 +1,22 @@
 from sqlalchemy.orm import Session
-from database.models import User, Drive, engine
+from database.models import User, Drive, engine, drive_passenger
 
 session = Session(engine)
 
 
-def get_or_create_user(chat_id, name, surname, phone_number):
+def get_or_create_user(chat_id):
     user = session.query(User).filter(User.chat_id == chat_id).first()
     if not user:
         user = User(chat_id=chat_id)
-    user.name = name
-    user.surname = surname
-    user.phone_number = phone_number
-    session.add(user)
-    session.commit()
-    return user
+        session.add(user)
+        session.commit()
+        return user, True
+    return user, False
 
 
-def edit_user(user, name=None, surname=None, phone_number=None, place_from=None, place_to=None, active_search=None):
-    if name:
-        user.name = name
-    if surname:
-        user.surname = surname
-    if phone_number:
-        user.phone_number = phone_number
-    if place_from:
-        user.place_from = place_from
-    if place_to:
-        user.place_to = place_to
-    if active_search is not None:
-        user.active_search = active_search
+def edit_user(user, attrs):
+    for key, value in attrs.items():
+        setattr(user, key, value)
     session.commit()
     return user
 
@@ -47,42 +35,66 @@ def create_drive(place_from, place_to, driver_id, max_passengers_amount, departu
     return drive
 
 
-def edit_drive(drive,
-               place_from=None,
-               place_to=None,
-               max_passengers_amount=None,
-               departure_time=None,
-               comment=None,
-               is_done=None):
-    if place_from:
-        drive.place_from = place_from
-    if place_to:
-        drive.place_to = place_to
-    if max_passengers_amount:
-        drive.max_passengers_amount = max_passengers_amount
-    if departure_time:
-        drive.departure_time = departure_time
-    if is_done is not None:
-        drive.is_done = is_done
-    if comment:
-        drive.comment = comment
+def edit_drive(drive, attrs):
+    for key, value in attrs.items():
+        setattr(drive, key, value)
     session.commit()
     return drive
 
 
-def drive_add_passenger(drive, passenger):
-    if passenger not in drive.passengers and drive.max_passengers_amount < len(drive.passengers):
+def drive_add_passenger(drive, passenger, num_of_passengers=1):
+    if passenger not in drive.passengers and drive.max_passengers_amount > drive.current_passengers_amount:
         drive.passengers.append(passenger)
+    drive.current_passengers_amount += num_of_passengers
     session.commit()
+    drive_pass = session.query(drive_passenger).filter_by(
+        drive_id=drive.id,
+        user_id=passenger.id
+    ).first()
+    drive_pass.passenger_count = num_of_passengers
+    session.commit()
+    print(drive_pass.id)
+    print(drive_pass.passenger_count)
     return drive
 
 
 def drive_delete_passenger(drive, passenger):
+    print(drive.current_passengers_amount)
     if passenger in drive.passengers:
+        drive_pass = session.query(drive_passenger).filter_by(drive_id=drive.id,user_id=passenger.id).first()
+        print(drive_pass.id)
+        print(drive_pass.passenger_count)
+        drive.current_passengers_amount -= drive_pass.passenger_count
         drive.passengers.remove(passenger)
+    print(drive.current_passengers_amount, "suka blya")
     session.commit()
     return drive
 
 
-def get_drive_by():
-    pass
+def get_drive_by(attrs):
+    drive = session.query(Drive)
+    for key, value in attrs.items():
+        drive = drive.filter(key == value)
+    return drive
+
+
+def get_all_drive():
+    return session.query(Drive).all()
+
+
+if __name__ == "__main__":
+    # some = ["da", "Da", "dA"]
+    # print(some.index("Da"))
+    # print(some[some.index("Da")])
+    driver, created = get_or_create_user(12354)
+    # drive = create_drive(place_from="da", place_to="da", driver_id=driver.id, max_passengers_amount=4, departure_time="123", comment="ads")
+    driver = edit_drive(driver, {"name": "Name", "surname": "Surname", "phone_number": "+380992345123", "max_passengers_amount": 4})
+    print(driver)
+    drive = get_drive_by({Drive.place_from:"da", Drive.place_to:"da"}).first()
+    drive = edit_drive(drive, {"current_passengers_amount": 0})
+    print(drive)
+    drive_add_passenger(drive, driver, 3)
+    drive_delete_passenger(drive, driver)
+    print(drive.passengers)
+    someshit = session.query(drive_passenger).all()
+    print(someshit)
--- database/models.py
+++ database/models.py
@@ -16,8 +16,18 @@ drive_passenger = Table(
     "drive_passenger",
     Base.metadata,
     Column("drive_id", Integer, ForeignKey("drive.id")),
-    Column("user_id", Integer, ForeignKey("user.id"))
+    Column("user_id", Integer, ForeignKey("user.id")),
+    Column("passenger_count", Integer)
 )
+
+
+# class DrivePassenger(Base):
+#     __tablename__ = "drive_passenger"
+#
+#     id = Column(Integer, autoincrement=True, primary_key=True)
+#     drive_id = Column(Integer, ForeignKey("drive.id")),
+#     user_id = Column(Integer, ForeignKey("user.id")),
+#     passenger_count = Column(Integer)
 
 
 class Drive(Base):
@@ -26,10 +36,11 @@ class Drive(Base):
     id = Column(Integer, autoincrement=True, primary_key=True)
     place_from = Column(String(255))
     place_to = Column(String(255))
+    passengers = relationship('User', secondary=drive_passenger, backref='drive_passenger')
     driver_id = Column(Integer, ForeignKey("user.id"))
-    driver = relationship("User", backref="drive")
-    max_passengers_amount = Column(Integer)
-    passengers = relationship('User', secondary=drive_passenger, backref='drive')
+    driver = relationship("User", backref="drive_driver")
+    max_passengers_amount = Column(Integer, default=4)
+    current_passengers_amount = Column(Integer, default=0)
     departure_time = Column(String(255))
     is_done = Column(Boolean, default=False)
     comment = Column(Text)
@@ -46,6 +57,7 @@ class User(Base):
     place_from = Column(String(255), nullable=True)
     place_to = Column(String(255), nullable=True)
     active_search = Column(Boolean, default=False)
+    num_of_passengers = Column(Integer, default=1)
 
 
 # Base.metadata.drop_all(engine)
