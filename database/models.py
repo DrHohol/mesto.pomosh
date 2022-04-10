9acaf2eea2bb97421a276842f71c368432c835c6
Sautin Dmytro <dimonsova1488@gmail.com>
2022-04-10 16:36:09 +0300

    fixed timezone

--- database/models.py
+++ database/models.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 
+import pytz
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, Text, String, Boolean, ForeignKey, DateTime, BigInteger
 from sqlalchemy.orm import Session, sessionmaker, declarative_base, relationship
@@ -27,7 +28,7 @@ class Drive(Base):
     departure_time = Column(DateTime, nullable=True)
     comment = Column(Text, nullable=True)
     regular = Column(Boolean, default=False)
-    creation_time = Column(DateTime, default=datetime.datetime.utcnow() + datetime.timedelta(hours=3))
+    creation_time = Column(DateTime(timezone=True), default=datetime.datetime.now(tz=pytz.timezone(zone='Europe/Kiev')))
 
 class User(Base):
     __tablename__ = "user"
@@ -40,7 +41,7 @@ class User(Base):
     place_to = Column(String(255), nullable=True)
     active_search = Column(Boolean, default=False)
     num_of_passengers = Column(Integer, default=1)
-    registration_time = Column(DateTime, default=datetime.datetime.utcnow() + datetime.timedelta(hours=3), nullable=True)
+    registration_time = Column(DateTime(timezone=True), default=datetime.datetime.now(tz=pytz.timezone(zone='Europe/Kiev')), nullable=True)
 
 # Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
